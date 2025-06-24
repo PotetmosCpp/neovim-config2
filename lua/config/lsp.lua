c337aae0cf1a337d7393a818c9500e1a84e8501d
= <=>
2025-06-24 14:57:52 +0200

    lsp stuff better

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,10 +2,10 @@ vim.lsp.config["clangd"] = {
 	cmd = { "clangd" },
 	filetypes = { "c", "cpp" }
 }
---[[vim.lsp.config["rust-analyzer"] = {
+vim.lsp.config["rust-analyzer"] = {
 	cmd = { "rust-analyzer" },
 	filetypes = { "rs", "rust" }
-}]]--
+}
 
 vim.lsp.enable("clangd")
 
@@ -13,20 +13,6 @@ vim.diagnostic.config({
 	virtual_text = true, -- this make error text show up why tf is it not on by default 😭
 	virtual_lines = false,
 	update_in_insert = true,
-})
-
---vim.diagnostic.enable(); -- this shit hella weird find out why shit aint working thanks 
-
--- gay fucking shit dude what
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "rust",
-	callback = function()
-		vim.lsp.start({
-			name = "rust-analyzer",
-			cmd = { "rust-analyzer" }
-			--root_dir = vim.fs.dirname(vim.fs.find({ "Cargo.toml", ".git" }, { upward = true })[1]),
-		})
-	end,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
