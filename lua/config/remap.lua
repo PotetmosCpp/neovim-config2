6be78b2919db124a4361791b90c1a648796af3fd
aet <alvaro.traelnes@gmail.com>
2025-06-29 13:49:33 +0200

    Add terminal remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -11,12 +11,14 @@ vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<leader>tt", vim.cmd.terminal)
+
 --vim.keymap.set({"i", "n"}, "<F5>", "w:\n:!cargo run\n")
 
-vim.keymap.set({"n"}, "<F5>", function()
-	--[[if vim.bo.modified then
-		vim.cmd("wa")
-	end]]--
+--[[vim.keymap.set({"n"}, "<F5>", function()
+	--if vim.bo.modified then
+		--vim.cmd("wa")
+	--end
 
 	if vim.fn.filereadable(vim.fn.getcwd() .. "/makefile") == 1 then
 		vim.cmd("!make")
@@ -29,4 +31,4 @@ vim.keymap.set({"n"}, "<F5>", function()
 	else
 		print("bitch")
 	end
-end)
+end)]]--
