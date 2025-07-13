4fad0bd5e91d7c60fdb4e409109ae4f18da10fb5
aet <alvaro.traelnes@gmail.com>
2025-07-13 13:38:17 +0200

    Make it so lualine shows macro stuff

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,25 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
+		-- thanks gemini lol
+		local function show_macro_recording()
+			local recording_register = vim.fn.reg_recording()
+			if recording_register == "" then
+				return "" -- Not recording, so return an empty string to hide the component
+			else
+				return "Recording @" .. recording_register
+			end
+		end
+
 		require('lualine').setup {
 			sections = {
 				lualine_a = {'mode'},
 				--lualine_b = {'branch', 'diff', 'diagnostics'},
 				lualine_b = {"diagnostics"},
-				lualine_c = { },
+				lualine_c = { {
+					show_macro_recording,
+					color = { fg = "#ff9e64" },
+				} },
 				--lualine_x = {'encoding', 'fileformat', 'filetype'},
 				--lualine_x = {'progress'},
 				lualine_x = { },
